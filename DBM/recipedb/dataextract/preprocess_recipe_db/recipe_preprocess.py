3fd2e25fe0a561325d8f21086bc8e8a3d01412c2
kuno <101792115+rjs6418@users.noreply.github.com>
2022-09-13 09:42:47 +0900

    주석 추가 수정

--- DBM/recipedb/dataextract/preprocess_recipe_db/recipe_preprocess.py
+++ DBM/recipedb/dataextract/preprocess_recipe_db/recipe_preprocess.py
@@ -1,20 +1,18 @@
 import pandas as pd
 
 
-def merge_ingredients_columns(x):
+def merge_ingredients_columns(ingredient_str):
     '''
         여러 식재료를 하나의 열에 리스트로 결합
         :param
-            including_ingredient : 바꿀 식재료를 포함시킬 단어 
-            toingredient : 바꿀 식재료 이름
-            noingredient1,2,3 : 제외할 단어
-        :action
-            지정된 식져료 단어를 일정 규칙에 맞게 변경 후 저장
+            ingredient_str : 식재료 문자열
+        :return
+            식재료 문자열을 리스트 형태로 리턴
     '''    
-    tttlist = []
+    ingredient_list = []
     for i in x:
-        tttlist.append(i)
-    return list(set(tttlist) - {'nan'})
+        ingredient_list.append(i)
+    return list(set(ingredient_list) - {'nan'})
 
 #각 열에 나뉘어 있는 식재료 이름을 하나의 열에 리스트 형태로 삽입
 recipe_df = pd.read_csv("./recipe10k_db_ud.csv").astype(str)
